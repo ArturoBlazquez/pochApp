d8e2407cb20f4cebd382ee44c9b53fb031eee74d
arturo <arturo.blazquezperez@gmail.com>
2025-12-20 02:42:24 +0100

    Added full score view

--- src/app/pocha/scores/scores.ts
+++ src/app/pocha/scores/scores.ts
@@ -2,13 +2,14 @@ import { Component, computed, output } from '@angular/core';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzSpaceModule } from 'ng-zorro-antd/space';
-import { calculateGameScore } from '../pochaCalculator';
+import { Bid, calculateBidScore, calculateGameScore } from '../pochaCalculator';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { GameStore } from '../game.store';
-import { NzAvatarComponent } from 'ng-zorro-antd/avatar';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 import { NzFlexModule } from 'ng-zorro-antd/flex';
-import { NzDividerComponent } from 'ng-zorro-antd/divider';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzTagModule } from 'ng-zorro-antd/tag';
 
 @Component({
   selector: 'scores-step',
@@ -18,24 +19,36 @@ import { NzDividerComponent } from 'ng-zorro-antd/divider';
     NzSpaceModule,
     NzTableModule,
     NzIconModule,
-    NzAvatarComponent,
+    NzAvatarModule,
     NzFlexModule,
-    NzDividerComponent,
+    NzDividerModule,
+    NzTagModule,
   ],
   templateUrl: './scores.html',
 })
 export class ScoresStep {
-  constructor(protected gameStore: GameStore) {
-  }
-
   next = output<void>();
   back = output<void>();
 
   scores = computed(() => calculateGameScore(this.gameStore.hands(), this.gameStore.currentHandIndex()));
-
   maxScoreIndex = computed(() => this.scores().indexOf(Math.max(...this.scores())))
+
+  constructor(protected gameStore: GameStore) {
+  }
 
   gameHasFinished() {
     return this.gameStore.getCurrentHandNumber() == this.gameStore.hands().length
+  }
+
+  preview = (bid: Bid) => {
+    return calculateBidScore(bid);
+  };
+
+  calculateScore(index: number) {
+    return calculateGameScore(this.gameStore.hands(), index)
+  }
+
+  getBid(handIndex: number, playerIndex: number) {
+    return this.gameStore.hands()[handIndex].bids[playerIndex];
   }
 }
